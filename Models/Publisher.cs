7acd47beddf6e6d4fda865b34f8a34fe957870b1
Truliman <93204114+Truliman@users.noreply.github.com>
2022-11-21 11:37:51 +0200

    1

--- Models/Publisher.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Buliga_Rares_Lab2.Models
-{
-    public class Publisher
-    {
-        public int ID { get; set; }
-        public string PublisherName { get; set; }
-        public ICollection<Book>? Books { get; set; }
-    }
-}
